3462630fec676ae88b1acf7ceae26a35f7693321
Darshan Heble <darshanheble@gmail.com>
2025-04-29 18:17:14 +0530

    added privy

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,55 @@
+import { usePrivy, useLogin, useLogout } from "@privy-io/react-auth";
+import { useEffect } from "react";
+
 const App = () => {
-  return <div>App</div>;
+  const { ready, authenticated, user } = usePrivy();
+  const { login } = useLogin();
+  const { logout } = useLogout();
+
+  useEffect(() => {
+    if (ready) {
+      login();
+    }
+  }, [!authenticated]);
+
+  if (!ready) {
+    return <div>Loading...</div>;
+  }
+
+  // Now it's safe to use other Privy hooks and state
+  return (
+    <div>
+      Privy is ready!
+      {authenticated ? (
+        <button onClick={logout}>Logout</button>
+      ) : (
+        <button onClick={login}>Login or Sign Up</button>
+      )}
+      {/* <button onClick={login}>Login or Sign Usp</button> */}
+      {authenticated ? (
+        <div>
+          <h2>Authenticated</h2>
+          <p>User ID: {user?.id}</p>
+
+          <p>Email: {user?.email?.address}</p>
+          {user?.isGuest ? <p>User is a guest</p> : <p>User is not a guest</p>}
+          {user?.hasAcceptedTerms ? (
+            <p>User has accepted Terms and Conditions and/or Privacy Policy </p>
+          ) : (
+            <p>
+              User has not accepted Terms and Conditions and/or Privacy Policy{" "}
+            </p>
+          )}
+          <p>guest: {user?.hasAcceptedTerms}</p>
+          <p>Created AT: {user?.createdAt.toDateString()}</p>
+        </div>
+      ) : (
+        <div>
+          <h2>Not Authenticated</h2>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+import { PrivyProvider } from "@privy-io/react-auth";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <PrivyProvider
+      appId="cma1etucc000djx0mwvfr4g8b"
+      // clientId=""
+      config={{
+        appearance: {
+          theme: "dark",
+          // Defaults to 'Log in or sign up'
+          landingHeader: "Log in or sign up to Creator CLub",
+          showWalletLoginFirst: true,
+        },
+        // Create embedded wallets for users who don't have a wallet
+        embeddedWallets: {
+          createOnLogin: "users-without-wallets",
+        },
+      }}
+    >
+      <App />
+    </PrivyProvider>
+  </StrictMode>
+);
